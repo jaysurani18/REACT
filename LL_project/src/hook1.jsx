6fd5fe7bc1f483dc89298a04541c9b465bd3cb4f
jaysurani18 <jaysurani008@gmail.com>
2026-01-20 09:16:00 +0530

    dynamically give value usinng checkbox

--- LL_project/src/hook1.jsx
+++ LL_project/src/hook1.jsx
@@ -1,17 +1,39 @@
-import React,{Component} from "react";
-export default function HookExample(){
-    const [count , setCount] = React.useState(0);
-    const [name,setName] = React.useState("Guest");
-    const [checked , setChecked] = React.useState(true);
-        return(
-        <div>
-            yes<input type="Checkbox" checked={checked} onChange={(e) => setChecked(e.target.checked)}></input>
-            <h1>Count: {count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
-            <h2>Hello, {name}!</h2>
-            <input type="Text" value={name} onChange={(e) => setName(e.target.value)}></input>
-        </div>
-        )
+import React, { useState } from 'react';
+export default function Hook2() {
+    const [user, setUser] = useState({
+        name: "Guest",
+        age: 25,
+        ce: true,
+        city: "Rajkot",
+    });
+    return <>
+    <label>Name: </label>
+    <input type="text" 
+    placeholder='Name'
+    value={user.name}
+    onChange={(e) => setUser({...user, name: e.target.value})}
+    />
+    <br/>
+    <label>Age: </label>
+    <input type="number" value={user.age}
+    onChange={(e) => setUser({...user, age: e.target.value})}
+    />
+    <br/>
+    <label>City: </label>
+    <input type="text" 
+    placeholder='City'
+    value={user.city}
+    onChange={(e) => setUser({...user, city: e.target.value})}
+    />
+    <br/>
+    <label>CE: </label>
+    <input type="checkbox" 
+    checked={user.ce}
+    onChange={(e) => setUser({...user, ce: e.target.checked})}
+    />
+    <h1>Name: {user.name}</h1>
+    <h1>Age: {user.age}</h1>
+    <h1>City: {user.city}</h1>
+    <h1>CE: {user.ce ? "Yes" : "No"}</h1>
+    </>
 }
-export {HookExample};   
