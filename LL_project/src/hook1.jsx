0307cac9d59b58ce1e3737a4dc485d1654177004
jaysurani18 <jaysurani008@gmail.com>
2026-01-20 08:59:16 +0530

    changes

--- LL_project/src/hook1.jsx
+++ LL_project/src/hook1.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-export default function Hook1() {
-  const [count, setCount] = useState(0);
-    return (
-        <div> 
-
-            <h1> Count: {count}</h1>
+import React,{Component} from "react";
+export default function HookExample(){
+    const [count , setCount] = React.useState(0);
+    const [name,setName] = React.useState("Guest");
+    const [checked , setChecked] = React.useState(true);
+        return(
+        <div>
+            yes<input type="Checkbox" checked={checked} onChange={(e) => setChecked(e.target.checked)}></input>
+            <h1>Count: {count}</h1>
             <button onClick={() => setCount(count + 1)}>Increment</button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <h2>Hello, {name}!</h2>
+            <input type="Text" value={name} onChange={(e) => setName(e.target.value)}></input>
         </div>
-    );
+        )
 }
+export {HookExample};   
